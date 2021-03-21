67b8a0b34e2607123a2e72e18e22eedcd098e52d
lucas <lucasssilva021@gmail.com>
2021-03-21 00:33:19 -0300

    Finalizado o layout de commit fazendo abrir o site do github ao clicar no commit

---
+++ app/src/main/java/com/silva021/githubrepository/adapter/CommitAdapter.java
@@ -0,0 +1,72 @@
+package com.silva021.githubrepository.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.silva021.githubrepository.databinding.LayoutCommitsBinding;
+import com.silva021.githubrepository.model.Repository;
+import com.silva021.githubrepository.model.RepositoryCommit;
+
+import java.util.List;
+
+public class CommitAdapter extends RecyclerView.Adapter<CommitAdapter.ViewHolder> {
+
+    List<RepositoryCommit> mRepositoryCommitsList;
+    Context mContext;
+    private OnItemClickListener mListener;
+
+    public CommitAdapter(List<RepositoryCommit> mRepositoryCommitsList, Context mContext) {
+        this.mRepositoryCommitsList = mRepositoryCommitsList;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutCommitsBinding.inflate(LayoutInflater.from(mContext), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommitAdapter.ViewHolder holder, int position) {
+        RepositoryCommit repositoryCommit = mRepositoryCommitsList.get(position);
+        holder.mBinding.txtDate.setText(repositoryCommit.getCommit().getCommitter().getDate().replace("T", " ").replace("Z", ""));
+        holder.mBinding.txtDescriptionCommit.setText(repositoryCommit.getCommit().getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRepositoryCommitsList.size();
+    }
+
+    public RepositoryCommit getRepositoryCommit(int position) {
+        return mRepositoryCommitsList.get(position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener mListener) {
+        this.mListener = mListener;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        LayoutCommitsBinding mBinding;
+
+        public ViewHolder(LayoutCommitsBinding mBinding) {
+            super(mBinding.getRoot());
+            this.mBinding = mBinding;
+
+            mBinding.layoutCommit.setOnClickListener(view -> {
+                if (mListener != null) {
+                    mListener.onItemClick(getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+}
--- app/src/main/java/com/silva021/githubrepository/ui/CommitsActivity.java
+++ app/src/main/java/com/silva021/githubrepository/ui/CommitsActivity.java
@@ -1,11 +1,16 @@
 package com.silva021.githubrepository.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.silva021.githubrepository.R;
+import com.silva021.githubrepository.adapter.CommitAdapter;
+import com.silva021.githubrepository.adapter.RepositoryAdapter;
 import com.silva021.githubrepository.api.GitHubService;
 import com.silva021.githubrepository.api.ServiceGenerator;
 import com.silva021.githubrepository.databinding.ActivityCommitsBinding;
@@ -19,9 +24,10 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class CommitsActivity extends AppCompatActivity {
+public class CommitsActivity extends AppCompatActivity implements CommitAdapter.OnItemClickListener{
     Repository mRepository;
     ActivityCommitsBinding mBinding;
+    private CommitAdapter mCommitAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +49,8 @@ public class CommitsActivity extends AppCompatActivity {
         ServiceGenerator.createService(GitHubService.class).getCommits(repository.getOwner().getLogin(), repository.getName()).enqueue(new Callback<List<RepositoryCommit>>() {
             @Override
             public void onResponse(Call<List<RepositoryCommit>> call, Response<List<RepositoryCommit>> response) {
-                Toast.makeText(CommitsActivity.this, "adasd", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(CommitsActivity.this, "adasd", Toast.LENGTH_SHORT).show();
+                initRecycler(response.body());
             }
 
             @Override
@@ -57,10 +64,21 @@ public class CommitsActivity extends AppCompatActivity {
         finish();
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
-
+    private void initRecycler(List<RepositoryCommit> list) {
+        mCommitAdapter = new CommitAdapter(list, getApplicationContext());
+        mCommitAdapter.setOnItemClickListener(this::onItemClick);
+        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mBinding.recyclerView.setAdapter(mCommitAdapter);
+    }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finishActivity();
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        RepositoryCommit repositoryCommit = mCommitAdapter.getRepositoryCommit(position);
+        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(repositoryCommit.getHtmlUrl())));
     }
 }
